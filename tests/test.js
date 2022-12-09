11cf2df18f1fc9b7fae31114e4cbc9bd51c1daa9
Dale Lane <dale.lane@uk.ibm.com>
2022-12-09 23:03:33 +0000

    tests: additional unit tests
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- tests/test.js
+++ tests/test.js
@@ -6,7 +6,7 @@ const mockKafkaCluster = require('./mockcluster');
 const resources = require('./testdata.json');
 
 
-describe('generator', function () {
+describe('generator', () => {
 
     let consumerStub;
     let adminStub;
@@ -19,7 +19,7 @@ describe('generator', function () {
     });
 
 
-    describe('#generateAsyncApi()', function () {
+    describe('#generateAsyncApi()', () => {
         it('should generate expected AsyncAPI docs', async () => {
             const TEST_CLUSTER = {
                 brokers: ['test-bootstrap.com:9092'],
@@ -39,6 +39,98 @@ describe('generator', function () {
 
                 assert.deepStrictEqual(result, resources[topic].asyncapi);
             }
+        });
+    });
+
+    describe('set security parameters', () => {
+        const topic = 'FLIGHT.LANDINGS';
+        const brokers = ['kafka-bootstrap:8092'];
+        const username = 'myuser';
+        const password = 'mypass';
+
+        it('should recognise unauthenticated', async () => {
+            const TEST_CLUSTER = {
+                brokers, topic,
+            };
+
+            const result = await generator.generateAsyncApi(TEST_CLUSTER);
+            assert.strictEqual(result.servers.kafka.protocol, 'kafka');
+            assert.strictEqual('security' in result.servers.kafka, false);
+            assert.strictEqual('securitySchemes' in result.components, false);
+        });
+
+        it('should recognise encrypted', async () => {
+            const TEST_CLUSTER = {
+                brokers, topic,
+                ssl: { rejectUnauthorized: false },
+            };
+
+            const result = await generator.generateAsyncApi(TEST_CLUSTER);
+            assert.strictEqual(result.servers.kafka.protocol, 'kafka-secure');
+            assert.deepStrictEqual(result.servers.kafka.security, [{ kafkaSecurity: [] }]);
+            assert.strictEqual(result.components.securitySchemes.kafkaSecurity.type, 'X509');
+        });
+
+        it('should recognise scram-sha-512', async () => {
+            const TEST_CLUSTER = {
+                brokers, topic,
+                sasl: {
+                    mechanism: 'scram-sha-512',
+                    username, password,
+                }
+            };
+
+            const result = await generator.generateAsyncApi(TEST_CLUSTER);
+            assert.strictEqual(result.servers.kafka.protocol, 'kafka');
+            assert.deepStrictEqual(result.servers.kafka.security, [{ kafkaSecurity: [] }]);
+            assert.strictEqual(result.components.securitySchemes.kafkaSecurity.type, 'scramSha512');
+        });
+
+        it('should recognise plain', async () => {
+            const TEST_CLUSTER = {
+                brokers, topic,
+                sasl: {
+                    mechanism: 'plain',
+                    username, password,
+                },
+            };
+
+            const result = await generator.generateAsyncApi(TEST_CLUSTER);
+            assert.strictEqual(result.servers.kafka.protocol, 'kafka');
+            assert.deepStrictEqual(result.servers.kafka.security, [{ kafkaSecurity: [] }]);
+            assert.strictEqual(result.components.securitySchemes.kafkaSecurity.type, 'plain');
+        });
+
+        it('should recognise sasl/plain', async () => {
+            const TEST_CLUSTER = {
+                brokers, topic,
+                sasl: {
+                    mechanism: 'plain',
+                    username, password,
+                },
+                ssl: true
+            };
+
+            const result = await generator.generateAsyncApi(TEST_CLUSTER);
+            assert.strictEqual(result.servers.kafka.protocol, 'kafka-secure');
+            assert.deepStrictEqual(result.servers.kafka.security, [{ kafkaSecurity: [] }]);
+            assert.strictEqual(result.components.securitySchemes.kafkaSecurity.type, 'plain');
+        });
+
+        it('should recognise scram-sha-512 with ssl', async () => {
+            const TEST_CLUSTER = {
+                brokers, topic,
+                ssl: true,
+                sasl: {
+                    mechanism: 'scram-sha-512',
+                    username, password,
+                }
+            };
+
+            const result = await generator.generateAsyncApi(TEST_CLUSTER);
+            assert.strictEqual(result.servers.kafka.protocol, 'kafka-secure');
+            assert.deepStrictEqual(result.servers.kafka.security, [{ kafkaSecurity: [] }]);
+            assert.strictEqual(result.components.securitySchemes.kafkaSecurity.type, 'scramSha512');
         });
     });
 });
